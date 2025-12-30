45a2b09c03323c181a8cf68686d4bf669b5711d7
Mateuszo236 <mateuszoselkowski@gmail.com>
2025-12-31 00:03:47 +0100

    Init HTTP server with health endpoint

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
 
-func main(){
-	fmt.Println("Hello wolrd")
+func healthHandler(w http.ResponseWriter, r *http.Request){
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "OK\n")
 }
 
+func main(){
+	fmt.Println("Hello world")
+	
+	http.HandleFunc("/health", healthHandler)
+
+	if err := http.ListenAndServe(":8080" , nil); err != nil {
+		log.Fatal(err)
+	}
+	
+}
